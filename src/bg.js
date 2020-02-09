b04b9600ce7b446d89f9fc73d7164b6c087ff9dd
kimsooyoung <tge1375@naver.com>
2020-02-10 00:21:38 +0900

    image link instead of static jpg

--- src/bg.js
+++ src/bg.js
@@ -1,13 +1,18 @@
 const jsMeem = document.querySelector(".meem-here");
 
-const IMG_NUMBER = 5;
+const IMG_NUMBER = 4;
+
+const imageUrls = [
+    'https://sayingimages.com/wp-content/uploads/that-look-back-to-work-meme.jpg',
+    'https://memesbams.com/wp-content/uploads/2017/10/42-Enjoy-Your-Weekend-Meme.jpg',
+    'https://pics.me.me/askme-about-my-weekend-one-more-time-working-weekend-52477103.png',
+    'https://pics.awwmemes.com/wifi-notworking-lam-notworking-wifi-not-working-milton-meme-53518097.png'
+]
 
 function paintImg(randomNumber){
-    const image = new Image();
     console.log(randomNumber);
-    jsMeem.src = `../img/${randomNumber + 1}.jpg`;
-    // jsMeem.classList.add("bg-image");
-    // jsMeem.prepend(image);
+    // jsMeem.src = `../img/${randomNumber + 1}.jpg`;
+    jsMeem.src = imageUrls[randomNumber + 1];
 }
 
 function makeRandNum(){
